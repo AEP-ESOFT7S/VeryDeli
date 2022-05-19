c6f035e763c59d180c2ce9c30e0bdcfd16b82efa
Sotoriva <joaosotoriva@hotmail.com>
2022-05-19 15:39:22 -0300

    git init

---
+++ src/main/java/br/com/aep/verydeli/VerydeliApplication.java
@@ -0,0 +1,13 @@
+package br.com.aep.verydeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerydeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerydeliApplication.class, args);
+	}
+
+}
